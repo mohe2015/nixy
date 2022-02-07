a7674aaa1869b09e2bfa01ab42fb380978eac30b
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-02-08 00:28:06 +0100

    Add state

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,7 @@
 use std::{
     iter::{Enumerate, Peekable},
     slice::Iter,
+    vec,
 };
 
 // https://wduquette.github.io/parsing-strings-into-slices/
@@ -67,9 +68,15 @@ pub struct NixToken<'a> {
     //pub location: SourceLocation,
 }
 
+enum NixLexerState {
+    Default,
+    String,
+}
+
 pub struct NixLexer<'a> {
     pub data: &'a [u8],
     pub iter: Peekable<Enumerate<Iter<'a, u8>>>,
+    state: Vec<NixLexerState>,
     line_start: bool,
 }
 
@@ -78,6 +85,7 @@ impl<'a> NixLexer<'a> {
         Self {
             data,
             iter: data.into_iter().enumerate().peekable(),
+            state: vec![NixLexerState::Default],
             line_start: true,
         }
     }
@@ -87,67 +95,74 @@ impl<'a> Iterator for NixLexer<'a> {
     type Item = NixToken<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self.iter.next() {
-            Some((offset, b'{')) => Some(NixToken {
-                token_type: NixTokenType::CurlyOpen,
-            }),
-            Some((offset, b'=')) => Some(NixToken {
-                token_type: NixTokenType::Assign,
-            }),
-            Some((offset, b'"')) => {
-                
-                
-            }
-            Some((offset, b'#')) if self.line_start => {
-                let end = self.iter.find(|(_, c)| **c == b'\n');
-                let comment = &self.data[offset..=end.map(|v| v.0).unwrap_or(usize::MAX)];
-                println!("{:?}", std::str::from_utf8(comment));
-                Some(NixToken {
-                    token_type: NixTokenType::SingleLineComment,
-                })
-            }
-            Some((offset, b' '))
-            | Some((offset, b'\t'))
-            | Some((offset, b'\r'))
-            | Some((offset, b'\n')) => {
-                loop {
-                    match self.iter.peek() {
-                        Some((_, b' ')) | Some((_, b'\t')) | Some((_, b'\r'))
-                        | Some((_, b'\n')) => {
-                            self.iter.next();
-                        }
-                        _ => break,
-                    }
+        match self.state.last() {
+            Some(NixLexerState::Default) => match self.iter.next() {
+                Some((offset, b'{')) => Some(NixToken {
+                    token_type: NixTokenType::CurlyOpen,
+                }),
+                Some((offset, b'=')) => Some(NixToken {
+                    token_type: NixTokenType::Assign,
+                }),
+                Some((offset, b'"')) => {
+                    todo!()
                 }
-                let whitespace = &self.data[offset..self.iter.peek().unwrap().0];
-                println!("{:?}", std::str::from_utf8(whitespace));
-                Some(NixToken {
-                    token_type: NixTokenType::Whitespace,
-                })
-            }
-            // this can be literally anything (path, ..)
-            Some((offset, b'a'..=b'z')) | Some((offset, b'A'..=b'Z')) | Some((offset, b'_')) => {
-                loop {
-                    match self.iter.peek() {
-                        Some((_, b'a'..=b'z'))
-                        | Some((_, b'A'..=b'Z'))
-                        | Some((_, b'0'..=b'9'))
-                        | Some((_, b'_'))
-                        | Some((_, b'\''))
-                        | Some((_, b'-')) => {
-                            self.iter.next();
-                        }
-                        _ => break,
-                    }
+                Some((offset, b'#')) if self.line_start => {
+                    let end = self.iter.find(|(_, c)| **c == b'\n');
+                    let comment = &self.data[offset..=end.map(|v| v.0).unwrap_or(usize::MAX)];
+                    println!("{:?}", std::str::from_utf8(comment));
+                    Some(NixToken {
+                        token_type: NixTokenType::SingleLineComment,
+                    })
                 }
-                let identifier = &self.data[offset..self.iter.peek().unwrap().0];
-                println!("{:?}", std::str::from_utf8(identifier));
-                Some(NixToken {
-                    token_type: NixTokenType::Identifier(identifier),
-                })
+                Some((offset, b' '))
+                | Some((offset, b'\t'))
+                | Some((offset, b'\r'))
+                | Some((offset, b'\n')) => {
+                    loop {
+                        match self.iter.peek() {
+                            Some((_, b' ')) | Some((_, b'\t')) | Some((_, b'\r'))
+                            | Some((_, b'\n')) => {
+                                self.iter.next();
+                            }
+                            _ => break,
+                        }
+                    }
+                    let whitespace = &self.data[offset..self.iter.peek().unwrap().0];
+                    println!("{:?}", std::str::from_utf8(whitespace));
+                    Some(NixToken {
+                        token_type: NixTokenType::Whitespace,
+                    })
+                }
+                // this can be literally anything (path, ..)
+                Some((offset, b'a'..=b'z'))
+                | Some((offset, b'A'..=b'Z'))
+                | Some((offset, b'_')) => {
+                    loop {
+                        match self.iter.peek() {
+                            Some((_, b'a'..=b'z'))
+                            | Some((_, b'A'..=b'Z'))
+                            | Some((_, b'0'..=b'9'))
+                            | Some((_, b'_'))
+                            | Some((_, b'\''))
+                            | Some((_, b'-')) => {
+                                self.iter.next();
+                            }
+                            _ => break,
+                        }
+                    }
+                    let identifier = &self.data[offset..self.iter.peek().unwrap().0];
+                    println!("{:?}", std::str::from_utf8(identifier));
+                    Some(NixToken {
+                        token_type: NixTokenType::Identifier(identifier),
+                    })
+                }
+                None => None,
+                _ => todo!(),
+            },
+            Some(NixLexerState::String) => {
+                todo!()
             }
-            None => None,
-            _ => todo!(),
+            None => todo!(),
         }
     }
 }
