ff3a65bfb05a9174b54960502f65579b0dfe8549
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-02-08 21:21:06 +0100

    Jo

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,23 @@
+use core::fmt;
+
 use crate::lexer::{NixToken, NixTokenType};
 use itertools::{multipeek, MultiPeek};
 
-#[derive(Debug)]
 pub enum AST<'a> {
     Select(Box<AST<'a>>, Box<AST<'a>>),
     Identifier(&'a [u8]),
+}
+
+impl<'a> fmt::Debug for AST<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Select(arg0, arg1) => f.debug_tuple("Select").field(arg0).field(arg1).finish(),
+            Self::Identifier(arg0) => f
+                .debug_tuple("Identifier")
+                .field(&std::str::from_utf8(arg0).unwrap().to_owned())
+                .finish(),
+        }
+    }
 }
 
 pub fn parse_attrpath<'a, I: Iterator<Item = NixToken<'a>>>(lexer: &mut MultiPeek<I>) -> AST<'a> {
@@ -18,11 +31,9 @@ pub fn parse_attrpath<'a, I: Iterator<Item = NixToken<'a>>>(lexer: &mut MultiPee
                     Some(a) => {
                         result = Some(AST::Select(Box::new(a), Box::new(AST::Identifier(id))))
                     }
-                    None => {
-                        result = Some(AST::Identifier(id))
-                    }
+                    None => result = Some(AST::Identifier(id)),
                 }
-                
+
                 lexer.next();
             }
             Some(NixToken {
@@ -36,6 +47,7 @@ pub fn parse_attrpath<'a, I: Iterator<Item = NixToken<'a>>>(lexer: &mut MultiPee
                 todo!()
             }
             _ => {
+                lexer.reset_peek();
                 break;
             }
         }
@@ -54,43 +66,124 @@ pub fn parse_bind<'a, I: Iterator<Item = NixToken<'a>>>(lexer: &mut MultiPeek<I>
                 break;
             }
             _ => {
+                lexer.reset_peek();
                 let attrpath = parse_attrpath(lexer);
+                match lexer.next() {
+                    Some(NixToken {
+                        token_type: NixTokenType::Assign,
+                    }) => {}
+                    _ => panic!("unexpected token"),
+                }
                 println!("{:?}", attrpath);
+                parse_expr(lexer);
+                match lexer.next() {
+                    Some(NixToken {
+                        token_type: NixTokenType::Semicolon,
+                    }) => {}
+                    _ => panic!("unexpected token"),
+                }
+
                 todo!();
             }
         }
     }
 }
 
-pub fn parse_let<'a, I: Iterator<Item = NixToken<'a>>>(lexer: &mut MultiPeek<I>) {
+pub fn parse_let<'a, I: Iterator<Item = NixToken<'a>>>(
+    lexer: &mut MultiPeek<I>,
+) -> Option<AST<'a>> {
     loop {
         match lexer.peek() {
             Some(NixToken {
                 token_type: NixTokenType::Identifier(b"in"),
             }) => {
                 lexer.next();
-                break;
+                break None;
             }
             _ => {
+                lexer.reset_peek();
                 parse_bind(lexer);
             }
         }
     }
 }
 
-pub fn parse_expr<'a, I: Iterator<Item = NixToken<'a>>>(lexer: &mut MultiPeek<I>) {
+pub fn parse_expr_simple<'a, I: Iterator<Item = NixToken<'a>>>(
+    lexer: &mut MultiPeek<I>,
+) -> Option<AST<'a>> {
+    match lexer.next() {
+        Some(NixToken {
+            token_type: NixTokenType::Identifier(id),
+        }) => Some(AST::Identifier(id)),
+        _ => {
+            // TODO FIXME return None
+            todo!();
+        }
+    }
+}
+
+pub fn parse_expr_select<'a, I: Iterator<Item = NixToken<'a>>>(
+    lexer: &mut MultiPeek<I>,
+) -> Option<AST<'a>> {
+    parse_expr_simple(lexer)
+}
+
+pub fn parse_expr_app<'a, I: Iterator<Item = NixToken<'a>>>(
+    lexer: &mut MultiPeek<I>,
+) -> Option<AST<'a>> {
+    let mut result: Option<AST<'a>> = None;
+    loop {
+        let jo = parse_expr_select(lexer);
+        match jo {
+            Some(expr) => {
+                match result {
+                    Some(a) => result = Some(AST::Select(Box::new(a), Box::new(expr))),
+                    None => result = Some(expr),
+                }
+
+                lexer.next();
+            }
+            None => {
+                break;
+            }
+        }
+    }
+    result
+}
+
+pub fn parse_expr_op<'a, I: Iterator<Item = NixToken<'a>>>(
+    lexer: &mut MultiPeek<I>,
+) -> Option<AST<'a>> {
+    parse_expr_app(lexer)
+}
+
+pub fn parse_expr_if<'a, I: Iterator<Item = NixToken<'a>>>(
+    lexer: &mut MultiPeek<I>,
+) -> Option<AST<'a>> {
+    parse_expr_op(lexer)
+}
+
+pub fn parse_expr_function<'a, I: Iterator<Item = NixToken<'a>>>(
+    lexer: &mut MultiPeek<I>,
+) -> Option<AST<'a>> {
     let token = lexer.next();
     match token.map(|t| t.token_type) {
         Some(NixTokenType::Identifier(b"let")) => {
             println!("letttt");
-            parse_let(lexer);
+            parse_let(lexer)
         }
-        _ => todo!(),
+        _ => parse_expr_if(lexer),
     }
-
-    assert_eq!(None, lexer.next())
 }
 
-pub fn parse<'a, I: Iterator<Item = NixToken<'a>>>(lexer: &mut I) {
+pub fn parse_expr<'a, I: Iterator<Item = NixToken<'a>>>(
+    lexer: &mut MultiPeek<I>,
+) -> Option<AST<'a>> {
+    let result = parse_expr_function(lexer);
+    assert_eq!(None, lexer.next());
+    result
+}
+
+pub fn parse<'a, I: Iterator<Item = NixToken<'a>>>(lexer: &mut I) -> Option<AST<'a>> {
     parse_expr(&mut multipeek(lexer))
 }
