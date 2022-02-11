8af4afbcb2de5d698d73582de2719ceff47ff7bb
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-02-12 00:11:17 +0100

    nice

--- java/IfClosure.java
+++ java/IfClosure.java
@@ -2,20 +2,12 @@ public class IfClosure implements NixObject {
 
 	@Override
 	public NixObject call(NixObject condition) {
-		return new NixObject() {
-			@Override
-			public NixObject call(NixObject trueCase) {
-				return new NixObject() {
-					@Override
-					public NixObject call(NixObject falseCase) {
-						NixBoolean evaluatedCondition = (NixBoolean) condition.call(null);
-						if (evaluatedCondition.value) {
-							return trueCase.call(null);
-						} else {
-							return falseCase.call(null);
-						}
-					}
-				};
+		return trueCase -> falseCase -> {
+			NixBoolean evaluatedCondition = (NixBoolean) condition.call(null);
+			if (evaluatedCondition.value) {
+				return trueCase.call(null);
+			} else {
+				return falseCase.call(null);
 			}
 		};
 	}
